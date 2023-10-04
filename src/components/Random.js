3f07527845775d58441e9af8fee919fc06a0ff3d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-05 00:58:46 +0600

    Update Random.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,1 +1,14 @@
 
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Random = () => {
+
+
+  const {gif, loading, fetchData} = useGif();
